f180ce12a963e24a30651993eef25f6e60ba84fa
Guilherme Soares <guilherme.soares@iovlabs.org>
2024-10-29 10:27:46 +0100

    feat: 3 - Customize Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive" size="sm">Click me</Button>
-      <p className="text-red-500 font-semibold">
-        Guilherme
-      </p>
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
